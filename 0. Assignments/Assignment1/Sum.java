5916fbf47524b1ad46358b211be0e2affa1d5c36
Nandhini L <86370900+NandhiniLakshmipathy@users.noreply.github.com>
2023-08-26 20:33:50 +0530

    Update Sum.java

--- 0. Assignments/Assignment1/Sum.java
+++ 0. Assignments/Assignment1/Sum.java
@@ -1,1 +1,16 @@
+class Sum{
 
+public static void main(String a[]){
+
+int num1 = 52;
+int num2 = 23;
+
+int sum = num1 + num2;
+
+if(sum % 2 != 0)
+    System.out.println("The sum is Odd");
+else
+    System.out.println("The sum is Even");
+
+}
+}
