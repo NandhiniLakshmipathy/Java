d906152a185ae2cfc8c0e452e4f9b6c2e56aa414
Nandhini L <86370900+NandhiniLakshmipathy@users.noreply.github.com>
2023-08-26 20:21:11 +0530

    Create Greaterno.java

---
+++ 0. Assignments/Assignment1/Greaterno.java
@@ -0,0 +1,14 @@
+class Greaterno{
+
+public static void main(String a[]){
+
+int num1 = 2;
+int num2 = 3;
+
+if(num1 > num2)
+    System.out.println(num1 + " is greater than " + num2);
+else
+    System.out.println(num2 + " is greater than " + num1);
+
+}
+}
