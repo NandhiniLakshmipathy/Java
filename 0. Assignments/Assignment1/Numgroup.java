63f0c02d503f45c04a684fb4cf34679a9f621b98
Nandhini L <86370900+NandhiniLakshmipathy@users.noreply.github.com>
2023-08-26 20:29:36 +0530

    Create Numgroup.java

---
+++ 0. Assignments/Assignment1/Numgroup.java
@@ -0,0 +1,15 @@
+class Numgroup{
+
+public static void main(String a[]){
+
+int num = 51;
+
+if(num <= 40)
+    System.out.println(num + " belongs to Group1");
+else if(num > 40 && num < 51)
+    System.out.println(num + " belongs to Group2");
+else
+    System.out.println(num + " belongs to Group3");
+
+}
+}
