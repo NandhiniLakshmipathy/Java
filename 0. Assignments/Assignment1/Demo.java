ae66f4ae8b313f3aab2fcdf9546e4cf9c54c6a6a
Nandhini L <86370900+NandhiniLakshmipathy@users.noreply.github.com>
2023-08-26 20:10:16 +0530

    Create Demo.java

---
+++ 0. Assignments/Assignment1/Demo.java
@@ -0,0 +1,12 @@
+class Demo{
+
+public static void main(String a[]){
+
+int num = 40;
+
+if(num % 10 == 0)
+    System.out.println(num + " is divisible by 10");
+else
+    System.out.println(num + " is not divisible by 10");
+}
+}
