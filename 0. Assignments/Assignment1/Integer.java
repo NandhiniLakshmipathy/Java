75a34bd47a6d583c864e68a9de51289fe90c9a45
Nandhini L <86370900+NandhiniLakshmipathy@users.noreply.github.com>
2023-08-26 20:17:59 +0530

    Create Integer.java

---
+++ 0. Assignments/Assignment1/Integer.java
@@ -0,0 +1,14 @@
+class Integer{
+
+public static void main(String a[]){
+
+int num = 0;
+
+if(num<0)
+    System.out.println(num + " is a Negative number");
+else if(num>0)
+    System.out.println(num + " is a Positive number");
+else
+    System.out.println("The number is Zero");
+}
+}
