de1d49b286ad2e76567e22217c1507322097e182
Nandhini L <86370900+NandhiniLakshmipathy@users.noreply.github.com>
2023-08-26 20:08:48 +0530

    Update Main.java

--- 0. Assignments/Assignment1/Main.java
+++ 0. Assignments/Assignment1/Main.java
@@ -1,1 +1,13 @@
+class Main{
 
+public static void main(String a[]){
+
+int num = 40;
+
+if(num % 2 == 0)
+     System.out.println(num + " is a Even number");
+else
+     System.out.println(num + " is a Odd number");
+
+}
+}
